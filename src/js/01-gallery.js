bc54295f6be955e6a2360a51a33634a9e7afdd9d
valentinaabakumova <abakumova.va13@gmail.com>
2022-05-28 20:44:32 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,41 +1,31 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
-// import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items';
 // Change code below this line
 
-// import SimpleLightbox from 'simplelightbox';
+console.log(galleryItems);
 
-// import 'simplelightbox/dist/simple-lightbox.min.css';
+const containerWithGalleryItems = document.querySelector('.gallery');
+const myMarkup = createMarkup(galleryItems);
 
-// import { galleryItems } from './gallery-items.js';
+containerWithGalleryItems.insertAdjacentHTML('afterbegin', myMarkup);
 
-// console.log(galleryItems);
+function createMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="item" style="list-style: none;">
+            <a class="gallery__item" href="${original}" onclick="event.preventDefault()">
+            <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+            </a>
+      </li>
+`;
+    })
+    .join('');
+}
 
-// Add imports above this line
-// import { galleryItems } from './gallery-items';
-// import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
-// Change code below this line
-
-// const containerWithGalleryItems = document.querySelector('.gallery');
-// const myMarkup = createMarkup(galleryItems);
-
-// containerWithGalleryItems.insertAdjacentHTML('afterbegin', myMarkup);
-
-// function createMarkup(galleryItems) {
-//   return galleryItems
-//     .map(({ preview, original, description }) => {
-//       return `
-//       <li class="item">
-//             <a class="gallery__item" href="${original}" onclick="event.preventDefault()">
-//             <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
-//             </a>
-//       </li>
-// `;
-//     })
-//     .join('');
-// }
-
-// const lightbox = new SimpleLightbox('.gallery a', {
-//   captionsData: 'alt',
-//   captionDelay: 250,
-// });
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+// import { throttle } from 'lodash';
+// var throttle = require('lodash.throttle');
 
+const form = document.querySelector('form');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+const label = document.querySelector('label');
+
+function formValue() {
+  const value = {
+    email: email.value,
+    message: message.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(value));
+}
+form.addEventListener('input', formValue);
+
+function formOutput() {
+  const getOutput = JSON.parse(localStorage.getItem('feedback-form-state'));
+  email.textContent = getOutput.email;
+  message.textContent = getOutput.message;
+}
+
+formOutput();
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  form.reset();
+  localStorage.removeItem('feedback-form-state');
+  //   message.textContent = '';
+  console.log(localStorage.getItem('feedback-form-state'));
+}
