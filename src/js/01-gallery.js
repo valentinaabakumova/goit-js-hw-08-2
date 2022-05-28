7b6695163242b960098ca6790bc8fef83b88d8f2
valentinaabakumova <abakumova.va13@gmail.com>
2022-05-28 19:21:53 +0200

    start 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,41 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+// import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// import SimpleLightbox from 'simplelightbox';
+
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// import { galleryItems } from './gallery-items.js';
+
+// console.log(galleryItems);
+
+// Add imports above this line
+// import { galleryItems } from './gallery-items';
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+// Change code below this line
+
+// const containerWithGalleryItems = document.querySelector('.gallery');
+// const myMarkup = createMarkup(galleryItems);
+
+// containerWithGalleryItems.insertAdjacentHTML('afterbegin', myMarkup);
+
+// function createMarkup(galleryItems) {
+//   return galleryItems
+//     .map(({ preview, original, description }) => {
+//       return `
+//       <li class="item">
+//             <a class="gallery__item" href="${original}" onclick="event.preventDefault()">
+//             <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+//             </a>
+//       </li>
+// `;
+//     })
+//     .join('');
+// }
+
+// const lightbox = new SimpleLightbox('.gallery a', {
+//   captionsData: 'alt',
+//   captionDelay: 250,
+// });
